0cdc8d2e47c7f86aaab4f508269acb63c110f618
lethabophenyo53-sketch <lethabophenyo53@gmail.com>
2026-03-31 21:24:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -102,4 +102,14 @@ window.onload = function () {
   checkLock();
   renderPage();
   setReminder();
+
+  function toggleMusic() {
+  const music = document.getElementById("bgMusic");
+
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+}
 };
