9ce4fc6bc3c7368d887303ee54b9ee717ac4bb4a
lethabophenyo53-sketch <lethabophenyo53@gmail.com>
2026-04-01 10:22:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 let currentPage = 0;
 
+function showPage() {
+  const pages = document.querySelectorAll(".page");
+
+  pages.forEach((p, i) => {
+    p.classList.remove("active", "prev");
+
+    if (i === currentPage) p.classList.add("active");
+    if (i === currentPage - 1) p.classList.add("prev");
+  });
+}
+
+function nextPage() {
+  const pages = document.querySelectorAll(".page");
+  if (currentPage < pages.length - 1) {
+    currentPage++;
+    showPage();
+  }
+}
+
+function prevPage() {
+  if (currentPage > 0) {
+    currentPage--;
+    showPage();
+  }
+}
+
 /* ---------------- SIGN UP ---------------- */
 function signup() {
   const email = document.getElementById("email").value;
